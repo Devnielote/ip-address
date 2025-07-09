960c917a999aefb6fcab89ab2bec2f36f5cad830
Devnielote <danielfc_26@hotmail.com>
2025-07-09 00:38:32 -0600

    chore: create initial react files and react routing structure

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function App() {
+  return(
+  <h1 className="">Hola React + TypeScript + Vite</h1>
+  )
+}
+
+export default App
