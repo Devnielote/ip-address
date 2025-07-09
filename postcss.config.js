a65090ac0d5d285f85af841e804a4c897a3f10cd
Devnielote <danielfc_26@hotmail.com>
2025-07-09 15:00:23 -0600

    chore: update postcss config file

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,7 @@
-import tailwindcss from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
+import tailwind from '@tailwindcss/postcss';
+import autoprefixer from 'autoprefixer';
 
 export default {
-  plugins: [
-    tailwindcss(),
-    autoprefixer(),
-  ],
-}
+  plugins: [tailwind(), autoprefixer()],
+};
+
